39b8e32d4241846fe4001f54664c14d91860b77e
Pupkov Egor <ferrguzz13@gmail.com>
2021-10-24 20:23:37 +0500

    Update index.php

--- index.php
+++ index.php
@@ -4,3 +4,7 @@
  $d=date("d.m.Y H:i");
  echo($d);
 ?>
+<ul class="menu">
+  <li><a href="lab-1-1.php">http://pupkov/WWW/lab-1-1.php</a></li>
+  <li><a href="lab-1-2.php">http://pupkov/WWW/lab-1-2.php</a></li>
+  <li><a href="lab-1-3.php">http://pupkov/WWW/lab-1-3.php</a></li>
