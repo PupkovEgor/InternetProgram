323b47c574e9458510e19e02024bd97c6aa85b95
Pupkov Egor <ferrguzz13@gmail.com>
2021-10-24 21:27:48 +0500

    Update index.php

--- index.php
+++ index.php
@@ -8,3 +8,4 @@
   <li><a href="http://pupkov/WWW/lab-1-1.php">Упражнение 1-2</a></li>
   <li><a href="http://pupkov/WWW/lab-1-2.php">Упражнение 1-3</a></li>
   <li><a href="http://pupkov/WWW/lab-1-3.php">Упражнение 1-4</a></li>
+  <li><a href="http://pupkov/WWW/lab-1-5.php">Самостоятельная работа</a></li>
