29a0817d02e8b14e7c8a6ba6fc98dce67569c526
Pupkov Egor <ferrguzz13@gmail.com>
2021-10-24 20:14:21 +0500

    Create lab-1-3.php

---
+++ lab-1-3.php
@@ -0,0 +1,13 @@
+<?php
+	const NUM_E = 2.71828;
+	echo ('Число е равно ' . NUM_E . '<br>');
+	$num_e1 = NUM_E;
+	echo ('$num_e1 = ' . $num_e1 . ' - ' . gettype($num_e1) . '<br>');
+	settype($num_e1, "string");
+	echo ('$num_e1 = ' . $num_e1 . ' - ' . gettype($num_e1) . '<br>');
+	settype($num_e1, "integer");
+	echo ('$num_e1 = ' . $num_e1 . ' - ' . gettype($num_e1) . '<br>');
+	settype($num_e1, "boolean");
+	echo ('$num_e1 = ' . $num_e1 . ' - ' . gettype($num_e1) . '<br>');
+?>
+	
