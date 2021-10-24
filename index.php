f73f22b89bcec0300e39a77818d357e764437a5a
Pupkov Egor <ferrguzz13@gmail.com>
2021-10-24 21:25:16 +0500

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
  echo($d);
 ?>
 <ul class="menu">
-  <li><a href="lab-1-1.php">http://pupkov/WWW/lab-1-1.php</a></li>
-  <li><a href="lab-1-2.php">http://pupkov/WWW/lab-1-2.php</a></li>
-  <li><a href="lab-1-3.php">http://pupkov/WWW/lab-1-3.php</a></li>
+  <li><a href="http://pupkov/WWW/lab-1-1.php">Упражнение 1-2</a></li>
+  <li><a href="http://pupkov/WWW/lab-1-2.php">Упражнение 1-3</a></li>
+  <li><a href="http://pupkov/WWW/lab-1-3.php">Упражнение 1-4</a></li>
