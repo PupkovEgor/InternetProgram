4ab2ac0096b15f3cd4c6da54fc947585836a5836
Pupkov Egor <ferrguzz13@gmail.com>
2021-10-25 00:28:38 +0500

    Update index.php

--- LR1/index.php
+++ LR1/index.php
@@ -8,4 +8,15 @@
   <li><a href="http://pupkov/WWW/lab-1-1.php">Упражнение 1-2</a></li>
   <li><a href="http://pupkov/WWW/lab-1-2.php">Упражнение 1-3</a></li>
   <li><a href="http://pupkov/WWW/lab-1-3.php">Упражнение 1-4</a></li>
-  <li><a href="http://pupkov/WWW/lab-1-5.php">Самостоятельная работа</a></li>
+  <li><a href="http://pupkov/WWW/lab-1-5.php">Самостоятельная работа №1</a></li>
+  <li><a href="http://pupkov/WWW/lab-2-1.php">Упражнение 1-6</a></li>
+  <li><a href="http://pupkov/WWW/lab-2-2.php">Упражнение 1-7</a></li>
+  <li><a href="http://pupkov/WWW/lab-2-3.php">Упражнение 1-8</a></li>
+  <li><a href="http://pupkov/WWW/lab-2-4.php">Упражнение 1-9</a></li>
+  <li><a href="http://pupkov/WWW/lab-2-5.php">Упражнение 1-10</a></li>
+  <li><a href="http://pupkov/WWW/lab-2-6.php">Упражнение 1-11</a></li>
+  <li><a href="http://pupkov/WWW/lab-2-7.php">Упражнение 1-12</a></li>
+  <li><a href="http://pupkov/WWW/lab-2-8.php">Упражнение 1-13</a></li>
+  <li><a href="http://pupkov/WWW/lab-2-9.php">Упражнение 1-14</a></li>
+  <li><a href="http://pupkov/WWW/lab-2-10.php">Самостоятельная работа №2-1</a></li>
+  <li><a href="http://pupkov/WWW/lab-2-11.php">Самостоятельная работа №2-2</a></li>
