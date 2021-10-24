89428f67b7f298a999970d030655bf4ae569a3a9
Pupkov Egor <ferrguzz13@gmail.com>
2021-10-24 22:36:43 +0500

    Update lab-2-4.php

--- LR1/lab-2-4.php
+++ LR1/lab-2-4.php
@@ -1,5 +1,13 @@
 <?php
-$var = 5; $i = 0;
-while (++$i <= $var)
- { echo $i . ' '; }
+$dn=rand(1,7);
+print ($dn ." - номер дня недели <br>");
+switch ($dn) {
+case 1: print ("это понедельник"); break;
+case 2: print ("это вторник"); break;
+case 3: print ("это среда"); break;
+case 4: print ("это четверг"); break;
+case 5: print ("это пятница"); break;
+case 6: print ("это суббота"); break;
+case 7: print ("это воскресенье");
+}
 ?>
